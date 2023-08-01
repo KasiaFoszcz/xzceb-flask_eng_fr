d6f8365e8acea23fbe4ce2c6cdcbb5ac75b61dcc
Kasia Foszcz <kasia3678@gmail.com>
2023-08-01 16:21:23 -0400

    Fix unsaved changes

--- final_project/server.py
+++ final_project/server.py
@@ -8,24 +8,20 @@ app = Flask("Web Translator")
 def englishToFrench():
     textToTranslate = request.args.get('textToTranslate')
     # Write your code here
-    english_to_french(textToTranslate)
-
-    return "Translated text to French: " + french_text
+    french_text = english_to_french(textToTranslate)
+    return f"Translated text to French: {french_text}"
 
 @app.route("/frenchToEnglish")
 def frenchToEnglish():
     textToTranslate = request.args.get('textToTranslate')
     # Write your code here
-    french_to_english(textToTranslate)
-    return "Translated text to English" + english_text
+    english_text = french_to_english(textToTranslate)
+    return f"Translated text to English {english_text}"
 
 @app.route("/")
 def renderIndexPage():
     # Write the code to render template
-    def home():
-   return render_template('index.html')
-if __name__ == '__main__':
-   app.run()
+    return render_template('index.html')
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8080)
